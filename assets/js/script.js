6f39a28a43907229fe4ed2ad328f8c06027256dc
Hamzah <82611798+brobro10000@users.noreply.github.com>
2022-02-06 18:26:00 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,3 @@ import { Element } from "./modules/class/Element.mjs";
 new Element('p', 'userInput', 'row p-4').append('body')
 
 new Element('h1').append('body').textContent = 'hello'
-
-let test = 'test'
